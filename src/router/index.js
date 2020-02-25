f2707583bcb5b80dc3272dfd41d55209c74745a8
Alex Reznichenko <alex.reznichenko789@gmail.com>
2020-02-25 15:00:10 +0200

    Add navigation bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import TodosPage from '../views/TodosPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'TodosPage',
+    component: TodosPage
   },
   {
     path: '/about',
